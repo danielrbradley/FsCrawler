54252e883a8d2115aabd35e6b51b3adfd8eab141
Daniel Bradley <daniel@danielbradley.net>
2016-07-09 00:16:23 +0100

    Initialize repository from ProjectScaffold

---
+++ src/FsCrawler/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsCrawler")>]
+[<assembly: AssemblyProductAttribute("FsCrawler")>]
+[<assembly: AssemblyDescriptionAttribute("F# Web Crawler")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
+    let [<Literal>] InformationalVersion = "1.0"
